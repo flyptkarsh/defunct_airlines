def43441fac1648de3bfbbd2347a10a852d14e54
flyptkarsh <ptkarsh@gmail.com>
2016-06-17 13:10:25 -0400

    added static pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,10 @@ Rails.application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
+   get 'about' => 'pages#about'
+   get 'contact' => 'pages#contact'
+   get 'blog' => 'pages#blog'
+   
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
